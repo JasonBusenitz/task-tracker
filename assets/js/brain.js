c0ee5b329514a34d037df79b9ff97f2da06ef488
Laura Schooley <laura.schooley3@gmail.com>
2024-07-26 14:45:56 -0500

    updated JS to sort

--- assets/js/brain.js
+++ assets/js/brain.js
@@ -1,6 +1,7 @@
 const taskField = document.getElementById("taskInput");
 const priorityField = document.getElementById("priorityInput");
 const savebutton = document.getElementById("savebutton");
+const taskTable = document.getElementById("task-list");
 savebutton.addEventListener("click", function () {
   const currentTasks = JSON.parse(localStorage.getItem("tasks")) || [];
   let task = taskField.value.trim();
@@ -9,6 +10,22 @@ savebutton.addEventListener("click", function () {
   localStorage.setItem("tasks", JSON.stringify(currentTasks));
   location.reload();
 });
+function renderAllTasks() {
+  const allTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  renderTasks(allTasks, "task-list",false);
+}
+renderAllTasks();
+
+taskTable.addEventListener("click", function(event){
+  if (event.target.matches("button")){
+    const indexToUpdate=parseInt(event.target.dataset.index);
+    const currentTasks=JSON.parse(localStorage.getItem("tasks"));
+    currentTasks[indexToUpdate].priority=event.target.dataset.priority;
+    localStorage.setItem("tasks", JSON.stringify(currentTasks));
+    location.reload();
+  }
+})
+
 
 /*Click Modal (event Listener) and a li is added with the given text*/
 
