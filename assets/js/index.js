f3d63d15205161f577f9718c13ecfab44579a417
Laura Schooley <laura.schooley3@gmail.com>
2024-07-25 19:56:39 -0500

    created Strike Out function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,37 @@
 //check local storage//
-let topPrio = JSON.parse(localStorage.getItem('top')) || [];
-let medPrio = JSON.parse(localStorage.getItem('med')) || [];
-let lowPrio = JSON.parse(localStorage.getItem('low'))|| [];
+// let topPrioValue = JSON.parse(localStorage.getItem("top")) || [];
+// let medPrioValue = JSON.parse(localStorage.getItem("med")) || [];
+// let lowPrioValue = JSON.parse(localStorage.getItem("low")) || [];
 
-const renderData = function(array,element){
-    if(!array.length) {
-        const noDataRow = document.createElement("tr");
-        const emptyTd = document.createElement("td");
-        const noDataEl = document.createElement("td");
-        noDataEl.textContent = "No Tasks";
-        noDataRow.appendChild(emptyTd);
-        noDataRow.appendChild(noDataEl);
-        document.getElementById(element).appendChild(noDataRow);
-        return;
-    };
-    for (let i = 0; i < array.length; i++) {
+let topPrioValue = ["To","Do","List"];
+let medPrioValue = ["To","Do","List"];
+let lowPrioValue = ["To","Do","List"];
+
+const renderData = function (array, element) {
+  if (!array.length) {
+    const noDataRow = document.createElement("tr");
+    const emptyTd = document.createElement("td");
+    const noDataEl = document.createElement("td");
+    noDataEl.textContent = "No Tasks";
+    noDataRow.appendChild(emptyTd);
+    noDataRow.appendChild(noDataEl);
+    document.getElementById(element).appendChild(noDataRow);
+    return;
+  }
+  for (let i = 0; i < array.length; i++) {
     const newRow = document.createElement("tr");
     const newCell = document.createElement("td");
     const newHeader = document.createElement("th");
-    newHeader.setAttribute("scope","row");
-    newHeader.textContent=i+1;
-    newCell.textContent=topPrio[i];
+    newHeader.setAttribute("scope", "row");
+    newHeader.textContent = i + 1;
+    newCell.textContent = topPrioValue[i];
+    newCell.classList.add("task-item");
     newRow.appendChild(newHeader);
     newRow.appendChild(newCell);
     document.getElementById(element).appendChild(newRow);
-};
+  }
 };
 
-renderData(topPrio, "topPrio");
-renderData(medPrio, "medPrio");
-renderData(lowPrio, "lowPrio")
+renderData(topPrioValue, "topPrio");
+renderData(medPrioValue, "medPrio");
+renderData(lowPrioValue, "lowPrio");
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,37 @@
-const brainDump = document.querySelector('name');
-const listType = document.querySelector('name');
+// const brainDump = document.querySelector('name');
+// const listType = document.querySelector('name');
 const lowPrio = document.querySelector('#low');
 const mediumPrio = document.querySelector('#medium');
 const topPrio = document.querySelector('#top');
+const tablesContainer = document.querySelector("#all-tables-container");
+// const saveListItem = document.querySelector('name');
 
-const saveListItem = document.querySelector('name');
 
+// saveListItem.addEventListener('click', () => {
+//     const listItem = document.querySelector('.name').value;
+//     listType.textContent = listItem;
+//     localStorage.setItem('list', listItem);
+// });
 
-saveListItem.addEventListener('click', () => {
-    const listItem = document.querySelector('.name').value;
-    listType.textContent = listItem;
-    localStorage.setItem('list', listItem);
-});
+// function displayUserInput() {
+//     const listFromLocalStorage = localStorage.getItem('list');
 
-function displayUserInput() {
-    const listFromLocalStorage = localStorage.getItem('list');
+//     if (listFromLocalStorage) {
+//         brainDump.textContent = 'name'
+//     } else {
+//         brainDump.textContent = 'No Items on List Yet!'
+//     }
+// }
 
-    if (listFromLocalStorage) {
-        brainDump.textContent = 'name'
-    } else {
-        brainDump.textContent = 'No Items on List Yet!'
+// displayUserInput();
+
+//strike out function
+
+function strikeOut(event) {
+    const target = event.target;
+    if (target.matches(".task-item")) {
+        target.setAttribute("class", "strikeout");
     }
-}
+};
 
-displayUserInput();
-
+tablesContainer.addEventListener("click",strikeOut);
